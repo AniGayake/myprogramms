5a9cc6dce0e9df48ee2117c65e664039bd1dbfce
ShubhangiGayake <shubhangigayake123@gmail.com>
2019-09-08 11:43:31 +0530

    First added in git

---
+++ hello world/520 hello world.c
@@ -0,0 +1,14 @@
+
+//** NAME:PRADYUMNA GAYAKE
+//** ROLL NO.:520
+//** DIV: E
+//** TITLE:HELLO WORLD 
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
